5ed443f639f4472e2c57813ae4f4bf1a9d373efe
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2013-09-26 21:09:40 -0400

    Fix test

--- test/integration/ca/mcgill/epsilon/GroovyShellServiceTests.groovy
+++ test/integration/ca/mcgill/epsilon/GroovyShellServiceTests.groovy
@@ -5,28 +5,32 @@ import org.junit.*
 
 class GroovyShellServiceTests {
 
-    def service
+    def service, ticketService
     
     @Before void setUp() {
          service = new GroovyShellService()
+         ticketService = new TicketService()
     }
 
     @Test void should_create_tickets_in_shell_script() {
+        def pending = ticketService.findAllPending()
+        assert pending.size() == 0
+        
         def id = service.evaluate("""
             package ca.mcgill.epsilon
 
             def bug = TicketType.findByKey('BUG')
             def user = User.findByUsername('user')
-            def assigned = TicketStatus.findByKey('ASSIGNED')
 
             def ticket = new Ticket(summary:'a summary', description:'a description', type:bug, priority:3, commissioner:user).save(flush:true)
 
             return ticket.id
         """)
         
-        def pending = Ticket.findAll("from Ticket as ticket where ticket.progress.size = 1 order by ticket.dateCreated")
-        
-        assert pending.size() == 1    
+        pending = ticketService.findAllPending()
+        assert pending.size() == 1
         assert pending.first().id == id
+        
+        Ticket.get(id).delete()
     }
 }
