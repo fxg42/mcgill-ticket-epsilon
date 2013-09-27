4cf5a897a945b4ccf2eddfec33bb7aea407e39d7
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2013-09-26 21:17:38 -0400

    reformat

--- grails-app/services/ca/mcgill/epsilon/GroovyShellService.groovy
+++ grails-app/services/ca/mcgill/epsilon/GroovyShellService.groovy
@@ -2,14 +2,14 @@ package ca.mcgill.epsilon
 
 class GroovyShellService {
 
-    static expose = ['jmx']
-    
-    def evaluate(String script) {
-        try {
-            def shell = new GroovyShell(this.class.classLoader)
-            shell.evaluate(script)
-        } catch (e) {
-            e.message
-        }
+  static expose = ['jmx']
+
+  def evaluate(String script) {
+    try {
+      def shell = new GroovyShell(this.class.classLoader)
+      shell.evaluate(script)
+    } catch (e) {
+      e.message
     }
+  }
 }
--- test/integration/ca/mcgill/epsilon/GroovyShellServiceTests.groovy
+++ test/integration/ca/mcgill/epsilon/GroovyShellServiceTests.groovy
@@ -5,32 +5,32 @@ import org.junit.*
 
 class GroovyShellServiceTests {
 
-    def service, ticketService
+  def service, ticketService
     
-    @Before void setUp() {
-         service = new GroovyShellService()
-         ticketService = new TicketService()
-    }
+  @Before void setUp() {
+    service = new GroovyShellService()
+    ticketService = new TicketService()
+  }
 
-    @Test void should_create_tickets_in_shell_script() {
-        def pending = ticketService.findAllPending()
-        assert pending.size() == 0
+  @Test void should_create_tickets_in_shell_script() {
+    def pending = ticketService.findAllPending()
+    assert pending.size() == 0
         
-        def id = service.evaluate("""
-            package ca.mcgill.epsilon
+    def id = service.evaluate("""
+      package ca.mcgill.epsilon
 
-            def bug = TicketType.findByKey('BUG')
-            def user = User.findByUsername('user')
+      def bug = TicketType.findByKey('BUG')
+      def user = User.findByUsername('user')
 
-            def ticket = new Ticket(summary:'a summary', description:'a description', type:bug, priority:3, commissioner:user).save(flush:true)
+      def ticket = new Ticket(summary:'a summary', description:'a description', type:bug, priority:3, commissioner:user).save(flush:true)
 
-            return ticket.id
-        """)
+      return ticket.id
+    """)
         
-        pending = ticketService.findAllPending()
-        assert pending.size() == 1
-        assert pending.first().id == id
-        
-        Ticket.get(id).delete()
-    }
+    pending = ticketService.findAllPending()
+    assert pending.size() == 1
+    assert pending.first().id == id
+    
+    Ticket.get(id).delete()
+  }
 }
